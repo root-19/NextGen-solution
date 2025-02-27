fdb2fd1c435ec5ed16a48998616b7ee986ef9c78
rens <wasieacuna@gmail.com>
2025-02-27 08:08:17 -0800

    push

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -35,7 +35,7 @@ function Messages() {
     }
 
     axios
-      .post("https://nextgen-backend-l70i.onrender.com/send-message", newMessage)
+      .post("https://nextgen-backend-l70i.onrender.com", newMessage)
       .then(() => {
         setNewMessage({ sender: "", subject: "", content: "" });
 
