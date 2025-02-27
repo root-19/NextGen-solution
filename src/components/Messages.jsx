c3efb6382986018f9870d6d82bd9b4623569fb30
Rens  Acuña <127023454+root-19@users.noreply.github.com>
2025-02-27 08:40:00 -0800

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -35,7 +35,7 @@ function Messages() {
     }
 
     axios
-    .post("https://nextgen-backend-l70i.onrender.com/send-message", newMessage, {
+    .post("https://nextgen-backend-cjum.onrender.com/send-message", newMessage, {
       headers: { "Content-Type": "application/json" },
     })
       .then(() => {
